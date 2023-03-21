bd6baedb3c914dcab96bfd39d87a91e7855ecd5b
NgocHaiDev <ngochai170294@gmail.com>
2023-03-21 09:43:32 +0700

    sc video 3

---
+++ video3/video3.js
@@ -0,0 +1,13 @@
+// let c = 5;
+// let d = 5.9;
+
+// let x = 5;
+// let y = 5;
+
+// console.log('Hello world from javascript : ' , x==y)
+
+let x = 5, y = 6, z = 7;
+
+console.log("test string: ",'x = ',x,'y = ', y,'z = ', z)
+
+
