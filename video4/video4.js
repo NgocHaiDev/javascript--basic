77905c7cff5dca605ebe8e0e7368d00dc96dbb21
NgocHaiDev <ngochai170294@gmail.com>
2023-03-22 09:46:01 +0700

    sc chapter 2 video4

---
+++ video4/video4.js
@@ -0,0 +1,9 @@
+let a = 'toi la string - chuoi'
+let text = `we are the so-called "viking" from the north.`
+
+console.log(text)
+console.log(a.length)
+console.log(a.trim().length)
+
+console.log( 4 + 5 + 'a')
+console.log('a' + 4 + 5)
