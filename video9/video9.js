c271b60db5f2ec4e8a24c2b5e79916b58a3a9211
NgocHaiDev <ngochai170294@gmail.com>
2023-03-27 16:46:03 +0700

    chapter 2 video 9

---
+++ video9/video9.js
@@ -0,0 +1,13 @@
+console.log('Hello world from HTML')
+//arr = [item1, item2, item3]
+let b = ['MU', 'Chelsea', 'Liverpool'];
+
+console.log('type of b : ', typeof b, b);
+console.log('first item : ', b[0] , 'second item : ', b[1], 'third item : ', b[2]);
+
+let family = [
+    {name: 'Eric', age: 25},
+    {name: 'Monica', age: 24}
+]
+
+console.log('my family : ', family);
