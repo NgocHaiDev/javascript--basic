85e961d5895676c01e4d75db2174a85939cfd6fc
NgocHaiDev <ngochai170294@gmail.com>
2023-03-28 11:56:53 +0700

    for loops js video 11

---
+++ video11/video11.js
@@ -0,0 +1,13 @@
+console.log('Hello world from HTML')
+
+let arr = ['MU', 'ManCity', 'Liver', 'Chelsea', 'MU', 'ManCity', 'Liver', 'Chelsea']
+for(let i = 0; i < arr.length; i++){
+    console.log('top : ', arr[i])
+}
+
+// var i = 0
+for(var i = 0; i < arr.length; i++){
+    console.log('check variable i = ', arr[i])
+}
+var i = 0
+console.log('check value i : ', i)
