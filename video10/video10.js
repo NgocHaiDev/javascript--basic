fbe5a53d96fd1ad34a767b3a2f148ce897ef06ac
NgocHaiDev <ngochai170294@gmail.com>
2023-03-28 11:09:22 +0700

    chapter 2 video 10

---
+++ video10/video10.js
@@ -0,0 +1,22 @@
+console.log('Hello world from HTML')
+
+let a = 5; // x <- y 
+let b = 5;
+let c = '5';
+console.log('compare a, b: ', 'a = ', a, 'b = ' , b , 'result : ', a == b , a == c , a === b, a === c)
+
+// === typeof, value
+// == value
+
+
+//empty, null, undefined
+//empty
+let d = []
+let e = {}
+console.log('check value empty : ',d,e)
+//undefined
+let f;
+console.log('check value undefined : ', f)
+//null
+let g = null
+console.log('check value null : ', g)
