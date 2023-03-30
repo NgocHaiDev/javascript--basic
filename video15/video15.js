db99cecb4afd3aa8bed75bf456b74ee50be00718
NgocHaiDev <ngochai170294@gmail.com>
2023-03-30 14:04:27 +0700

    chapter 3 video15 declare functions

---
+++ video15/video15.js
@@ -0,0 +1,18 @@
+console.log('Hello world from HTML')
+
+//input parameter
+
+// let a = 5
+// let b = 5
+// function sum(a,b) {
+//  return a + b;
+// }
+
+// console.log('Result : ', sum(a,b))
+
+function sum(a,b) {
+ return a + b;
+}
+let c = sum(10,20)
+console.log('Result a + b : ', sum(6,9))
+console.log('Result c : ', c)
